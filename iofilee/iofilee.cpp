47a8797aabb4adcfeb6a6a1fdc77003f57acce9d
Aulianurfitria15 <aulianurfitria17@gmail.com>
2024-06-04 17:37:17 +0700

    menambahkan variabel global

--- iofilee/iofilee.cpp
+++ iofilee/iofilee.cpp
@@ -3,34 +3,3 @@
 #include <string>
 using namespace std;
 
-int main() {
-	string baris;
-
-	//membuka file dalam mode menulis.
-	ifstream outfile;
-	//menunjuk ke sebuah nama file
-	outfile.open("contohfile.txt");
-
-	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
-
-	//unlimited loop untuk menulis
-	while (true) {
-		cout << "- ";
-		//mendapatkan setiap karakter dalam satu baris
-		getline(cin, baris);
-		//loop akan berhenti jika anda memasukan karakter q
-		if (baris == "q") break;
-		//menulis dan memasukkan nilai dari 'baris' ke dalam file
-		outfile << baris << endl;
-	}
-	//selesai dalam menulis sekarang tutup filenya
-	outfile.close();
-
-	//membuka file dalam mode membaca
-	ifstream infile;
-	//menunjukan ke sebuah file
-	infile.open("contohfile.txt");
-
-	cout << endl << ">= Membuka dan membaca file " << endl;
-	//jika dile ada maka
-}
